1c15a2501393e316caedd40ca2aebbe75f78bfab
Tom-Stilwell <ThomasRStilwell@gmail.com>
2024-10-09 20:19:42 -0400

    fetch records from backend /api/data endpoint

--- data-review-assignment/src/components/DataReviewTable.tsx
+++ data-review-assignment/src/components/DataReviewTable.tsx
@@ -1,11 +1,20 @@
 // components/DataReview.tsx
 
 import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function DataReviewTable() {
 
+    const [records, setRecords] = useState([]);
+
     useEffect(() => {
+        axios.get("/api/data")
+        .then(res => {
+            setRecords(res.data.records);
+        })
     }, []);
+
+    console.log(records);
 
     return (
         <div style={{ margin: "20px" }}>
